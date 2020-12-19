d12838e20d7b99286838286f60c680e0d4934ba5
rajesh <rajesh>
2020-12-19 22:41:16 +0530

    first commit

---
+++ project-1/1.js
@@ -0,0 +1,5 @@
+function findSum(a, b) {
+    var sum = a + b;
+    console.log("sum is", sum);
+}
+findSum(123, 234);
---
+++ project-1/1.ts
@@ -0,0 +1,13 @@
+
+
+
+//create student datatype
+interface Student{
+    name:string;
+    age:number;
+    course:string;
+}
+
+
+
+let stdObj:Student={name:"ravi",age:20,course:"IT"};
